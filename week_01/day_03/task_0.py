90eb92d586211924d5789162825a0948e74a0409
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-03-12 17:10:35 +0700

    Redisign transliterate() function using str.replace()

--- week_01/day_03/task_0.py
+++ week_01/day_03/task_0.py
@@ -1,18 +1,13 @@
-import re
-
-
-translit_dict = {
+# transliteration dictionaries for transformation from russian into english
+one_letter = {
     "а": "a",
     "б": "b",
     "в": "v",
     "г": "g",
     "д": "d",
     "е": "e",
-    "ё": "jo",
-    "ж": "zh",
     "з": "z",
     "и": "i",
-    "й": "j",
     "к": "k",
     "л": "l",
     "м": "m",
@@ -24,45 +19,81 @@ translit_dict = {
     "т": "t",
     "у": "u",
     "ф": "f",
-    "х": "kh",
     "ц": "c",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "sch",
     "ъ": "\"",
     "ы": "y",
     "ь": "\'",
+}
+two_letter = {
+    "ё": "jo",
+    "ж": "zh",
+    "й": "jj",
+    "х": "kh",
+    "ч": "ch",
+    "ш": "sh",
     "э": "eh",
     "ю": "ju",
     "я": "ja",
 }
-ru_special_consonants = 'йъь'
-ru_iotated_vovels = 'еёюя'
+three_letter = {
+    "щ": "shh",
+}
+
+
+def dict_replacement(text, *args):
+    """ Function that applies replacement rules from replacement dictionaries.
+
+    Args:
+        text (str): text for replacement application
+        *args: list of dictionaries with replacement rules
+            Each dictionary contains replacement rules in following way:
+            {
+                "old_string" : "new_string"
+            }
+
+    Returns:
+        new_text (str): text after the replacement rules applied
+    """
+    list_of_dicts = args
+    new_text = text
+    for d in list_of_dicts:
+        for k, v in d.items():
+            new_text = new_text.replace(k.lower(), v.lower())
+            new_text = new_text.replace(k.upper(), v.upper())
+    return new_text
+
+
+def transliterate(text, source_lang="ru"):
+    """Transliteration function based on GOST http://transliteration.ru/gost/
+
+    Supports transliteration from Russian and vice versa
+
+    Args:
+        text (str): text for transliteration
+        source_lang (str): register-free marker of source language.
+            "ru", "rus", "r", "russain" for Russian source language
+            "eng", "en", "e", "english" for English source language
+
+    Returns:
+        eng_to_rus (str): russian text gathered from translit
+            OR
+        rus_to_eng (str): translitted text
+    """
+    if source_lang.lower() in ("eng", "en", "e", "english"):
+        # switch keys and values in replacement dictionaries
+        # because source dictionaries are for translit from russian into english
+        three_letter_r = {v: k for k, v in three_letter.items()}
+        two_letter_r = {v: k for k, v in two_letter.items()}
+        one_letter_r = {v: k for k, v in one_letter.items()}
+        # apply replacement rules from dictionaries
+        eng_to_rus = dict_replacement(text, three_letter_r, two_letter_r, one_letter_r)
+        return eng_to_rus
+    elif source_lang.lower() in ("ru", "rus", "r", "russain"):
+        # apply replacement rules from dictionaries
+        rus_to_eng = dict_replacement(text, three_letter, two_letter, one_letter)
+        return rus_to_eng
+    else:
+        raise ValueError("Wrong language selected")
 
 
 
-def transliterate(text, dictionary=translit_dict):
-    """"""
-    # split words with any space symbol
-    words = re.split(r'\s+', text)
-    # replace words with '{}' to save spacing format of text
-    space_template = re.sub(r'\S*', '{}', text)
-    trans_words = list()
-    # check language
-    for word in words:
-        # do transliteration following dictionary
-        # if language of text is russian
-
-        # procedure each word symbol by symbol checking if it uppercase
-        # if symbol not in transliteration dict, it stays unchanged
-        trans_letters = [dictionary[l.lower()] if l.islower()
-                         else dictionary[l.lower()].upper() if l.isupper()
-                         else l
-                         for l in word]
-        trans_words.append("".join(trans_letters))
-        # if language of text is english
-        # PLACE FOR CODE
-    return space_template.format(*trans_words)
-
-
-print(transliterate("Ё,Ж х;ч:Ш щъыыЬ ЭюЮюЯЯ"))
--- week_01/day_03/task_0_tests.py
+++ week_01/day_03/task_0_tests.py
@@ -5,12 +5,67 @@ from .task_0 import transliterate
 class TranslitTest(TestCase):
     """"""
     def test_ru_into_eng(self):
-        print(transliterate("ЁЖ хчШ щъыыЬ ЭюЮюЯЯ"))
-        print("JOZH khchSH sch\"yy\' EH juJUjuJAJA\n")
-        self.assertEqual(transliterate("ЁЖ хчШ щъыыЬ ЭюЮюЯЯ"), "JOZH khchSH sch\"yy\' EHjuJUjuJAJA")
+        """
+        Test different Russian letters
+        """
+        input_data = "абвЁЖ хчШ щъыыь ЭюЮюЯЯ"
+        expected = "abvJOZH khchSH shh\"yy\' EHjuJUjuJAJA"
+        self.assertEqual(transliterate(input_data), expected)
 
     def test_ru_into_eng_punct(self):
-        print(transliterate("Ё,Ж х;ч:Ш щъыыЬ ЭюЮюЯЯ"))
-        print("JO,ZH kh;ch:SH sch\"yy\' EHjuJUjuJAJA\n")
-        self.assertEqual(transliterate("Ё,Ж х;ч:Ш щъыыЬ ЭюЮюЯЯ"), "JO,ZH kh;ch:SH sch\"yy\' EHjuJUjuJAJA")
+        """
+        Test punctuation marks
+        """
+        input_data = "Ё,Ж х;ч:Ш щъыыь ЭюЮюЯЯ"
+        expected = "JO,ZH kh;ch:SH shh\"yy\' EHjuJUjuJAJA"
+        self.assertEqual(transliterate(input_data), expected)
+
+    def test_ru_into_eng_spaces(self):
+        """
+        Test space symbols
+        """
+        input_data = "   Ё ,Ж\t\nх;ч:Ш щъыыь\nЭюЮюЯЯ"
+        expected = "   JO ,ZH\t\nkh;ch:SH shh\"yy\'\nEHjuJUjuJAJA"
+        self.assertEqual(transliterate(input_data), expected)
+
+    def test_eng_into_ru(self):
+        """
+        Test from translit into Russian
+        """
+        input_data = "JOZH khchSH shh\"yy\' EHjuJUjuJAJA"
+        expected = "ЁЖ хчШ щъыыь ЭюЮюЯЯ"
+        self.assertEqual(transliterate(input_data, source_lang="eng"), expected)
+
+    def test_eng_rus(self):
+        """
+        Test backward compatibility
+        """
+        a = "   Ё ,Ж\t\tх;ч:Ш щъыы ь\tЭюЮюЯЯ"
+        b = transliterate(a)  # en
+        c = transliterate(b, source_lang='en')  # ru
+        self.assertEqual(a, c)
+        self.assertEqual(transliterate(a), b)
+        self.assertEqual(b, transliterate(c))
+
+    def test_eng_to_eng(self):
+        """
+        Test no changes if selected Russian language for English text
+        """
+        a = "qaSDGVD  ergs SDEGbsd"
+        self.assertEqual(a, transliterate(a, source_lang='ru'))
+
+    def test_rus_to_rus(self):
+        """
+        Test no changes if selected English language for Russian text
+        """
+        a = "Ё ,Ж\t\tх;ч:Ш щъыы ь\tЭюЮюЯЯ"
+        self.assertEqual(a, transliterate(a, source_lang='english'))
+
+    def test_wrong_language(self):
+        """
+        Test correct exception if wrong language selected
+        """
+        with self.assertRaises(ValueError):
+            transliterate('abcd', source_lang='British')
+
 
