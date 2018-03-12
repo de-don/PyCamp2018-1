ccd807876b9663e4b7368689a6cd3956fd276480
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-03-12 17:12:19 +0700

    Test function counts unique combinations of key:value in list of dicts

---
+++ week_01/day_03/task_1_tests.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+from .task_1 import keys_counter
+
+
+class KeyCounterTest(TestCase):
+    """"""
+    def test_simple_case(self):
+        input = [{'id': 1, 'success': True, 'name': 'Lary'},
+                 {'id': 2, 'success': False, 'name': 'Rabi'},
+                 {'id': 3, 'success': True, 'name': 'Alex'}]
+        expect = [{'key': 'id', 'value': 1, 'count': 1},
+                  {'key': 'success', 'value': True, 'count': 2},
+                  {'key': 'name', 'value': 'Lary', 'count': 1},
+                  {'key': 'id', 'value': 2, 'count': 1},
+                  {'key': 'success', 'value': False, 'count': 1},
+                  {'key': 'name', 'value': 'Rabi', 'count': 1},
+                  {'key': 'id', 'value': 3, 'count': 1},
+                  {'key': 'name', 'value': 'Alex', 'count': 1}]
+
+        self.assertEqual(keys_counter(input), expect)
+
+    def test_empty_list(self):
+        self.assertEqual(keys_counter([]), [])
+
+    def test_uniq_dicts(self):
+        input = [{'id': 1, 'success': True, 'name': 'Lary'},
+                 {'id': 2, 'success': False, 'name': 'Rabi'},]
+        expect = [{'key': 'id', 'value': 1, 'count': 1},
+                  {'key': 'success', 'value': True, 'count': 1},
+                  {'key': 'name', 'value': 'Lary', 'count': 1},
+                  {'key': 'id', 'value': 2, 'count': 1},
+                  {'key': 'success', 'value': False, 'count': 1},
+                  {'key': 'name', 'value': 'Rabi', 'count': 1},]
+
+        self.assertEqual(keys_counter(input), expect)
+
+    def test_similar_dicts(self):
+        input = [{'id': 1, 'success': True, 'name': 'Lary'},
+                 {'id': 2, 'success': True, 'name': 'Lary'},]
+        expect = [{'key': 'id', 'value': 1, 'count': 1},
+                  {'key': 'success', 'value': True, 'count': 2},
+                  {'key': 'name', 'value': 'Lary', 'count': 2},
+                  {'key': 'id', 'value': 2, 'count': 1},]
+
+        self.assertEqual(keys_counter(input), expect)
