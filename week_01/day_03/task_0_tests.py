c34284f2aa8a5a94fa6b81760b7959ed83cdc11e
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-03-13 18:19:15 +0700

    Redisigned using setUp() methdo

--- week_01/day_03/task_0_tests.py
+++ week_01/day_03/task_0_tests.py
@@ -3,68 +3,86 @@ from .task_0 import transliterate
 
 
 class TranslitTest(TestCase):
-    """"""
-    def test_ru_into_eng(self):
-        """
-        Test different Russian letters
-        """
-        input_data = "абвЁЖ хчШ щъыыь ЭюЮюЯЯ"
-        expected = "abvJOZH khchSH shh\"yy\' EHjuJUjuJAJA"
-        self.assertEqual(transliterate(input_data), expected)
+    def setUp(self):
+        self.simple_ru_string = "абвёж хчш щъыыь эюююяя"
+        self.simple_eng_string = "abvjozh khchsh shh\"yy\' ehjujujujaja"
+        self.uppercase_ru_string = "аБвЁж хЧш Щъыыь эюЮюяя"
+        self.uppercase_eng_string = "aBvJOzh khCHsh SHH\"yy\' ehjuJUjujaja"
+        self.uppercase_eng_string2 = "aBvJozh khChsh Shh\"yy\' ehjuJujujaja"
+        self.punctuation_ru_string = "Ё,Ж х;ч:Ш щъыыь ЭюЮюЯЯ"
+        self.punctuation_eng_string = "JO,ZH kh;ch:SH shh\"yy\' EHjuJUjuJAJA"
+        self.space_ru_string = "   Ё ,Ж\t\nх;ч:Ш щъыыь\nЭюЮюЯЯ"
+        self.space_eng_string = "   JO ,ZH\t\nkh;ch:SH shh\"yy\'\nEHjuJUjuJAJA"
 
-    def test_ru_into_eng_punct(self):
-        """
-        Test punctuation marks
-        """
-        input_data = "Ё,Ж х;ч:Ш щъыыь ЭюЮюЯЯ"
-        expected = "JO,ZH kh;ch:SH shh\"yy\' EHjuJUjuJAJA"
-        self.assertEqual(transliterate(input_data), expected)
+    def test_simple_ru_into_eng(self):
+        self.assertEqual(
+            transliterate(self.simple_ru_string),
+            self.simple_eng_string,
+        )
 
-    def test_ru_into_eng_spaces(self):
-        """
-        Test space symbols
-        """
-        input_data = "   Ё ,Ж\t\nх;ч:Ш щъыыь\nЭюЮюЯЯ"
-        expected = "   JO ,ZH\t\nkh;ch:SH shh\"yy\'\nEHjuJUjuJAJA"
-        self.assertEqual(transliterate(input_data), expected)
+    def test_uppercase_ru_into_eng(self):
+        self.assertEqual(
+            transliterate(self.uppercase_ru_string),
+            self.uppercase_eng_string,
+        )
+        self.assertNotEqual(
+            transliterate(self.uppercase_ru_string),
+            self.simple_eng_string,
+        )
+        self.assertNotEqual(
+            transliterate(self.uppercase_ru_string),
+            self.uppercase_eng_string2,
+        )
 
-    def test_eng_into_ru(self):
-        """
-        Test from translit into Russian
-        """
-        input_data = "JOZH khchSH shh\"yy\' EHjuJUjuJAJA"
-        expected = "ЁЖ хчШ щъыыь ЭюЮюЯЯ"
-        self.assertEqual(transliterate(input_data, source_lang="eng"), expected)
+    def test_punctuation_ru_into_eng(self):
+        self.assertEqual(
+            transliterate(self.punctuation_ru_string),
+            self.punctuation_eng_string,
+        )
 
-    def test_eng_rus(self):
-        """
-        Test backward compatibility
-        """
-        a = "   Ё ,Ж\t\tх;ч:Ш щъыы ь\tЭюЮюЯЯ"
-        b = transliterate(a)  # en
-        c = transliterate(b, source_lang='en')  # ru
+    def test_space_symbols_ru_into_eng(self):
+        self.assertEqual(
+            transliterate(self.space_ru_string),
+            self.space_eng_string,
+        )
+
+    def test_simple_eng_into_ru(self):
+        self.assertEqual(
+            transliterate(self.simple_eng_string, source_lang="eng"),
+            self.simple_ru_string,
+        )
+
+    def test_uppercase_eng_into_ru(self):
+        self.assertEqual(
+            transliterate(self.uppercase_eng_string, source_lang="eng"),
+            self.uppercase_ru_string,
+        )
+        self.assertEqual(
+            transliterate(self.uppercase_eng_string2, source_lang="eng"),
+            self.uppercase_ru_string,
+        )
+
+    def test_backward_compatability_rut_eng_rus(self):
+        a = self.space_ru_string
+        b = transliterate(a)  # ru to en
+        c = transliterate(b, source_lang='en')  # en to ru
         self.assertEqual(a, c)
         self.assertEqual(transliterate(a), b)
         self.assertEqual(b, transliterate(c))
 
-    def test_eng_to_eng(self):
-        """
-        Test no changes if selected Russian language for English text
-        """
-        a = "qaSDGVD  ergs SDEGbsd"
-        self.assertEqual(a, transliterate(a, source_lang='ru'))
+    def test_no_changes_eng_text_ru_lang(self):
+        self.assertEqual(
+            self.simple_eng_string,
+            transliterate(self.simple_eng_string, source_lang='ru')
+        )
 
-    def test_rus_to_rus(self):
-        """
-        Test no changes if selected English language for Russian text
-        """
-        a = "Ё ,Ж\t\tх;ч:Ш щъыы ь\tЭюЮюЯЯ"
-        self.assertEqual(a, transliterate(a, source_lang='english'))
+    def test_no_changes_ru_text_eng_lang(self):
+        self.assertEqual(
+            self.space_ru_string,
+            transliterate(self.space_ru_string, source_lang='english')
+        )
 
-    def test_wrong_language(self):
-        """
-        Test correct exception if wrong language selected
-        """
+    def test_wrong_source_language(self):
         with self.assertRaises(ValueError):
             transliterate('abcd', source_lang='British')
 
