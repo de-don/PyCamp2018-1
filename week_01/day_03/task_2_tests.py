dca3b6fca348eaa938344dd57fb747e38ecac7ee
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-03-13 18:22:10 +0700

    Redisigned using setUp() method

--- week_01/day_03/task_2_tests.py
+++ week_01/day_03/task_2_tests.py
@@ -4,22 +4,21 @@ from .task_2 import dict_merger
 
 class DictMergerTest(TestCase):
     """"""
-    def test_simple_case(self):
-        d1 = dict(one=1, two=2)
-        d2 = dict(four=1, three=3)
-        d3 = dict(one=1, two=2, three=3, four=1)
-        self.assertEqual(dict_merger(d1, d2), d3)
+    def setUp(self):
+        self.d1 = dict(one=1, two=2)
+        self.d2 = dict(four=1, three=3)
+        self.d3 = dict(one=1, two=2, three=3, four=1)
+        self.d4 = dict(one=5, three=3)
+        self.d5 = dict(one=5, two=2, three=3)
+        self.d6 = dict(one=1, two=2, three=3)
+        self.d7 = dict()
 
-    def test_different_vals(self):
-        d1 = dict(one=1, two=2)
-        d2 = dict(one=5, three=3)
-        d3 = dict(one=5, two=2, three=3)
-        d4 = dict(one=1, two=2, three=3)
-        self.assertEqual(dict_merger(d1, d2), d3)
-        self.assertNotEqual(dict_merger(d1, d2), d4)
+    def test_different_dicts(self):
+        self.assertEqual(dict_merger(self.d1, self.d2), self.d3)
 
-    def test_empty_dict(self):
-        d1 = dict()
-        d2 = dict(one=5, three=3)
-        d3 = dict(one=5, three=3)
-        self.assertEqual(dict_merger(d1, d2), d3)
+    def test_partial_merge(self):
+        self.assertEqual(dict_merger(self.d1, self.d4), self.d5)
+        self.assertNotEqual(dict_merger(self.d1, self.d4), self.d6)
+
+    def test_merge_with_empty_dict(self):
+        self.assertEqual(dict_merger(self.d7, self.d4), self.d4)
