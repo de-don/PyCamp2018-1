07a122ebc5bef6d3e6e539b142cd08f40a028e89
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-03-13 18:21:43 +0700

    F string used in logging.warning()

--- week_01/day_03/task_2.py
+++ week_01/day_03/task_2.py
@@ -3,9 +3,12 @@ import logging
 
 def dict_merger(dict1, dict2):
     """Function merges two Python dictionaries
-    Based on https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
-    If two dictionaries have same keys with the same values, merged one contains key: value pair without changes.
-    If two dictionaries have same keys with different values, merged one contains key: value pair from the second one.
+    Based on https://stackoverflow.com/questions/38987/
+    how-to-merge-two-dictionaries-in-a-single-expression
+    If two dictionaries have same keys with the same values,
+    merged one contains key: value pair without changes.
+    If two dictionaries have same keys with different values,
+    merged one contains key: value pair from the second one.
 
     Returns:
         dict(): merged dictionary
@@ -16,8 +19,8 @@ def dict_merger(dict1, dict2):
 
     for k in dict1.keys():
         if k in dict2.keys():  # and dict1[k] == dict2[k]:
-            logging.warning('\'{}: {}\' from dict1 will be replaced by '
-                            '\'{}: {}\' from dict2'.format(k, dict1[k], k, dict2[k]))
+            logging.warning(f'\'{k}: {dict1[k]}\' from dict1 will be replaced '
+                            f'by \'{k}: {dict2[k]}\' from dict2')
     return {**dict1, **dict2}
 
 
