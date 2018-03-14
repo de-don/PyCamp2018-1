4ad0633711c6c8e71b4e7788e69deec6c341fe89
fr0mhell <t1m0feev.grigorij@gmail.com>
2018-03-15 00:27:01 +0700

    Simple test of searcher

--- the_searcher/the_searcher_tests.py
+++ the_searcher/the_searcher_tests.py
@@ -1,7 +1,28 @@
 from unittest import TestCase
-from .the_searcher import the_searcher
+from click.testing import CliRunner
+
+from .the_searcher import searcher
+
+
+# global runner
+runner = CliRunner()
 
 
 class TheSearcherTests(TestCase):
     """"""
     def setUp(self):
+        self.pattern1 = "\w+@[\w.-_]+"
+        self.file = "the_searcher/emails.txt"
+
+    def test_zero_exit_code(self):
+        result1 = runner.invoke(
+            searcher,
+            ['-u', self.pattern1],
+            input="cgarcia@outlook.com\nblahblahblah\tjrif.kin@icloud.com",
+        )
+        result2 = runner.invoke(
+            searcher,
+            ['-u', self.pattern1, self.file],
+        )
+        self.assertEqual(result1.exit_code, 0)
+        self.assertEqual(result2.exit_code, 0)
