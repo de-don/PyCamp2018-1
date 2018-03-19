e4c2d6b5325cc7ded942f8a285eb29f748d144e5
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-03-19 18:20:11 +0700

    __getattr__ and __setattr__ add and test

--- task_5/custom_dictionaries.py
+++ task_5/custom_dictionaries.py
@@ -1,24 +1,41 @@
 class ReadOnlyDictionary:
-    """Class allows to transform Python dict() into attributes
+    """Class transforms dict() keys into attributes and gives access to dict()
+    values using attributes
 
     """
     def __init__(self, dictionary):
         if not isinstance(dictionary, dict):
             raise TypeError('Input data must Python dict()')
 
-        self._dictionary = dictionary
+        self._dictionary_of_attributes = dictionary
 
         cls = type(self)
 
         # transform internal dictionary into ReadOnlyDictionary
-        for key, value in self._dictionary.items():
-            if isinstance(self._dictionary[key], dict):
-                self._dictionary[key] = cls(value)
+        for key, value in self._dictionary_of_attributes.items():
+            if isinstance(self._dictionary_of_attributes[key], dict):
+                self._dictionary_of_attributes[key] = cls(value)
 
-        # get list of proper attribute names
-        self._attribute_names = tuple(self._dictionary.keys())
+    @property
+    def dictionary_of_attributes(self):
+        """Displays dictionary of attributes"""
+        return self._dictionary_of_attributes
 
-    # def __getattr__(self, item):
+    def __getitem__(self, item):
+        if item in self.dictionary_of_attributes.keys():
+            return self.dictionary_of_attributes[item]
+        raise IndexError
+
+    def __getattr__(self, item):
+        if item in self.dictionary_of_attributes.keys():
+            return self.dictionary_of_attributes[item]
+        raise AttributeError('No such attribute')
+
+    def __setattr__(self, name, value):
+        if name in self.dictionary_of_attributes.keys():
+            raise AttributeError('Attribute is read-only')
+        super().__setattr__(name, value)
+
 
 
 
--- task_5/custom_dictionaries_test.py
+++ task_5/custom_dictionaries_test.py
@@ -2,24 +2,53 @@ from unittest import TestCase
 from .custom_dictionaries import ReadOnlyDictionary as RODict
 
 
-class ReadOnlyDictionaryTests(TestCase):
+class CustomDictionariesTests(TestCase):
     def setUp(self):
         self.data1 = {'one': 1, 'two': 2}
-        self.data2 = {'one': 1, 'two': {'three': 3, 'four': 4}}
+        self.dict1 = RODict(self.data1)
 
-    def test_init_ro_dict(self):
+        self.data2 = {'one': 1, 'two': {'three': 3, 'four': 4}}
+        self.dict2_field2 = 'two'
+        self.dict2 = RODict(self.data2)
+
+        self.data3 = {'one': 1, 'two': 2}
+        self.dict3 = RODict(self.data1)
+
+    def test_mydict_init(self):
         with self.assertRaises(TypeError):
             RODict(['one', 1, 'two', 2])
-        # transforms dict() values into self type()
-        r_o_dict2 = RODict(self.data2)
-        self.assertTrue(isinstance(
-            r_o_dict2._dictionary['two'],
-            RODict)
-        )
+
+        with self.assertRaises(TypeError):
+            RODict(['one', 'two', 'three'])
 
         # check tuple of attribute names
-        r_o_dict1 = RODict(self.data1)
-        self.assertEqual(r_o_dict1._attribute_names, tuple(self.data1.keys()))
+        self.assertEqual(
+            self.dict1.dictionary_of_attributes.keys(),
+            self.data1.keys()
+        )
+
+        # check transformation dict() values into type custom dict
+        self.assertEqual(
+            type(self.dict2.dictionary_of_attributes[self.dict2_field2]),
+            type(self.dict2)
+        )
+
+    def test_my_dict_get_attribute(self):
+        self.assertEqual(
+            self.dict1.one,
+            self.data1['one']
+        )
+        self.assertEqual(
+            self.dict1.two,
+            self.data1['two']
+        )
+        with self.assertRaises(AttributeError):
+            a = self.dict1.three
+
+    def test_my_dict_set_attribute(self):
+        self.dict3.three = 3
+        self.assertTrue(self.dict3.three)
+
 
 
 
