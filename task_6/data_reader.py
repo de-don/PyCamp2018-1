215758c8609a6218fdfae8e26521cd54bbfdceed
fr0mhell <t1m0feev.grigorij@gmail.com>
2018-03-21 16:42:40 +0700

    Add cast of types for entries

--- task_6/data_reader.py
+++ task_6/data_reader.py
@@ -1,6 +1,6 @@
 import csv
-from datetime import datetime
 from dateutil import parser
+from datetime import datetime
 
 
 class Data:
@@ -8,7 +8,7 @@ class Data:
 
     """
 
-    _conversion_types = [float, int, bool, datetime]
+    _conversion_types = [int, float, parser.parse]
 
     def __init__(self):
         self._headers = list()
@@ -16,6 +16,12 @@ class Data:
 
     def __len__(self):
         return len(self._entries)
+
+    def __getitem__(self, item):
+        return self._entries[item]
+
+    def __iter__(self):
+        return iter(self._entries)
 
     def _add_headers(self, list_of_headers):
         self._headers += list_of_headers
@@ -29,8 +35,21 @@ class Data:
 
     def _add_entry(self, entry_values):
         # convert types of values
+        casted_entry_vals = list()
+        for val in entry_values:
+            # try each of probable types to cast
+            for cast_type in self._conversion_types:
+                if self._represents_some_type(cast_type, val):
+                    after_cast = cast_type(val)
+                    if isinstance(after_cast, datetime):
+                        after_cast = after_cast.date()
+                    casted_entry_vals.append(after_cast)
+                    break
+            # or do not cast and save as sting
+            else:
+                casted_entry_vals.append(val)
 
-        entry = dict(zip(self._headers, entry_values))
+        entry = dict(zip(self._headers, casted_entry_vals))
         self._entries.append(entry)
 
     @classmethod
@@ -46,9 +65,7 @@ class Data:
 
             # get entries from csv
             for row in reader:
-                print(row)
                 data._add_entry(row)
-
         return data
 
     def _entry_repr(self, entry):
@@ -64,5 +81,19 @@ class Data:
             entry_repr = self._entry_repr(self._entries[i])
             entries_repr.append(f'{i}:\n{entry_repr}')
         return '\n'.join(entries_repr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- task_6/data_reader_tests.py
+++ task_6/data_reader_tests.py
@@ -1,12 +1,31 @@
 from unittest import TestCase
 from .data_reader import Data
+from datetime import datetime, date
 
 
 class DataReaderTest(TestCase):
 
-    def test_read_csv(self):
+    def test_data_get_csv(self):
         d = Data().get_csv('table.csv')
-        print(d)
+        self.assertTrue(isinstance(d, Data))
+
+    def test_data_len(self):
+        d = Data().get_csv('table.csv')
+        self.assertTrue(len(d) == 3)
+
+    def test_data_iterable(self):
+        d = Data().get_csv('table.csv')
+        for entry in iter(d):
+            self.assertTrue(isinstance(entry, dict))
+
+    def test_type_cast(self):
+        d = Data().get_csv('table.csv')
+        headers = d._headers
+        entry = d[0]
+        self.assertTrue(isinstance(entry[headers[0]], str))
+        self.assertTrue(isinstance(entry[headers[1]], int))
+        self.assertTrue(isinstance(entry[headers[2]], str))
+        self.assertTrue(isinstance(entry[headers[3]], date))
 
 
 
